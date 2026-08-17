c68efe75c1596122921dfd17cafddc9cfcab61ef
MariaJaqueline-Germano <mariajaqueline2306@gmail.com>
2026-08-17 10:29:07 -0300

    cadastro.py
    Troca de LOOP para FUNÇÃO.

--- cadastro.py
+++ cadastro.py
@@ -17,16 +17,14 @@ def validacao(msg):
 
         # Repetição para Cadastro do Produto.
 
-while True:
-
+def menu_produtos():
     print("1 - Cadastrar produto")
-    
     opcao = input("Digite uma opção: ")
     print(opcao)
-
-        # Inserção de Dados.
-
+    
+    # Inserção de Dados.
     if opcao == "1":
+        print("Você escolheu cadastrar um produto!")
         
         id = validacao(("Digite o ID do produto: ")).upper()
         nome = validacao(("Nome do produto: ")).upper()
