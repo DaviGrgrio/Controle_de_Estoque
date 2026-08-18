f5efb96786c06fe1abb955345e2223d848eac536
MLuizaGS <mluizzags@gmail.com>
2026-08-18 18:01:48 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,24 +1,29 @@
 from cadastro import cadastrar
-from estoque import (buscar_produto, listar_produtos, adicionar_quantidade, retirar_quantidade, excluir produto)
+from estoque import (buscar_produto, listar_produtos, adicionar_quantidade, retirar_quantidade, excluir_produto)
 
 estoque = {}
 print("\n---------Controle de Estoque----------")
     
 print("|Digite 1 para cadastrar o produto   |")
 print("|Digite 2 para consultar o estoque   |")
-print("|Digite 3 para adicionar a quantidade|")
-print("|Digite 4 para retirar a quantidade  |")
-print("|Digite 5 para sair                  |")
+print("|Digite 3 para buscar o produto      |")
+print("|Digite 4 para adicionar a quantidade|")
+print("|Digite 5 para retirar a quantidade  |")
+print("|Digite 6 para excluir o produto     |")
+print("|Digite 0 para sair                  |")
 print("--------------------------------------")
 opcao = input("\nDigite uma das opções: ")
 
 if opcao == "1":
     cadastrar()
 elif opcao == "2": 
-    listar_produtos(estoque)    
+    buscar_produtos()    
 elif opcao == "3":
-    a
-
+    adicionar_produto()
+elif opcao == "4":
+    retirar_quantidade()
+elif opcao == "5":
+    excluir_produto()
     
     
     
