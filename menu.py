8c60122be6edd2a7221b4d343f1ad2d425898ee1
Davig. <oliveiradavi144@gmail.com>
2026-08-19 02:02:32 -0300

    Corrigi menu.py para ser compatível com estoque.py e cadastro.py
    - adicionei ciclo de repetição While para manter o menu aberto mesmo após executar uma ação;
    - ao iniciar já carrega o estoque salvo com carregar_estoque();
    - corrigi o nome das funções que estavam escritas erradas acarretando em um syntaxError;
    - pede o id_produto, quantidade, etc antes de realizar ações como buscar, adicionar ou remover produtos

--- menu.py
+++ menu.py
@@ -1,36 +1,53 @@
 from cadastro import cadastrar
-from estoque import (buscar_produto, listar_produtos, adicionar_quantidade, retirar_quantidade, excluir_produto)
+from estoque import (buscar_produto, listar_produtos, adicionar_quantidade, retirar_quantidade, excluir_produto, carregar_estoque)
 
-estoque = {}
-print("\n---------Controle de Estoque----------")
-    
-print("|Digite 1 para cadastrar o produto   |")
-print("|Digite 2 para consultar o estoque   |")
-print("|Digite 3 para buscar o produto      |")
-print("|Digite 4 para adicionar a quantidade|")
-print("|Digite 5 para retirar a quantidade  |")
-print("|Digite 6 para excluir o produto     |")
-print("|Digite 0 para sair                  |")
-print("--------------------------------------")
-opcao = input("\nDigite uma das opções: ")
+    #Carrega o estoque salvo
+estoque = carregar_estoque()
 
-if opcao == "1":
-    cadastrar()
-elif opcao == "2":
-    listar_produtos()
-elif opcao == "3": 
-    buscar_produtos()    
-elif opcao == "4":
-    adicionar_produto()
-elif opcao == "5":
-    retirar_quantidade()
-elif opcao == "6":
-    excluir_produto()
-elif opcao == "0":
-    print("\nSaindo do sistema...")
-    break
-else:
-    print("\nOpção invalida. Digite uma opção válida.")
+while True:
+    print("\n---------Controle de Estoque----------")
+        
+    print("|Digite 1 para cadastrar o produto   |")
+    print("|Digite 2 para consultar o estoque   |")
+    print("|Digite 3 para buscar o produto      |")
+    print("|Digite 4 para adicionar a quantidade|")
+    print("|Digite 5 para retirar a quantidade  |")
+    print("|Digite 6 para excluir o produto     |")
+    print("|Digite 0 para sair                  |")
+    print("--------------------------------------")
+    opcao = input("\nDigite uma das opções: ")
     
-    
+    if opcao == "1":
+        cadastrar(estoque)
+        
+    elif opcao == "2":
+        listar_produtos(estoque)
+        
+    elif opcao == "3": 
+        id_produto = input("Digite o ID do produto: ")    
+        buscar_produto(estoque, id_produto)
+            
+    elif opcao == "4":
+        id_produto = input("Digite o ID do produto: ")
+        quantidade = int(input("Digite a quantidade a adicionar: "))
+        adicionar_quantidade(estoque, id_produto, quantidade)
+        
+    elif opcao == "5":
+        id_produto = input("Digite o ID do produto: ")
+        quantidade = int(input("Digite a quantidade a retirar: "))
+        retirar_quantidade(estoque, id_produto, quantidade)
+        
+    elif opcao == "6":
+        id_produto = input("Digite o ID do produto: ")
+        excluir_produto(estoque, id_produto)
+        
+    elif opcao == "0":
+        print("\nSaindo do sistema...")
+        break
+        
+    else:
+        print("\nOpção invalida. Digite uma opção válida.")
+        
+        
+        
     
