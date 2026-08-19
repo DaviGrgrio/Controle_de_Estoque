4224199b1e9ac6a3189af617fbeab83ebc5198ba
Davig. <oliveiradavi144@gmail.com>
2026-08-19 01:49:30 -0300

    Corrigi cadastro.py para aceitar dicionário compartilhado
    - Função (validacao_numerica) que valida e transforma valores numéricos;
    - cadastrar() não recebia nada, agora recebe o estoque como parâmetro;
    - "estoque[id]" ao invés de "estoque = []" para armazenar produtos no dicionário e não em uma lista;
    - adiciona data_validade e ultima_atualizacao ao produto
    - usa a função já definida em estoque.py "salvar_estoque()" ao invés de criar uma nova

--- cadastro.py
+++ cadastro.py
@@ -1,7 +1,8 @@
+
         # Importação para GitHub através do JSON.
 import json
-print(1)
-estoque = []
+
+from estoque import salvar_estoque
 
         # Função para validar a resposta do usuário.
 
@@ -14,10 +15,19 @@ def validacao(msg):
 
         else:
             return dado
+            
+def validacao_numerica(msg, tipo=int):
+    while True:
+        dado = validacao(msg)
+        try:
+            return tipo(dado)
+        except ValueError:
+            print("Digite um número válido!")
+
 
         # Repetição para Cadastro do Produto.
 
-def cadastrar():
+def cadastrar(estoque):
         print("Você escolheu cadastrar um produto!\n")
         
         id = validacao(("Digite o ID do produto: ")).upper()
@@ -26,14 +36,14 @@ def cadastrar():
         codigo_de_barras = validacao(("Digite aqui o código de barras desse produto: ")).upper()
         corredor = validacao(("Digite aqui o corredor desse produto: ")).upper()
         prateleira = validacao(("Digite aqui a prateleira desse produto: ")).upper()
-        quantidade = validacao(("Digite aqui a quantidade que foi reposta no estoque desse produto: ")).upper()
-        estoque_minimo = validacao(("Digite aqui o estoque mínimo desse produto: ")).upper()
-        estoque_maximo = validacao(("Digite aqui o estoque máximo desse produto: ")).upper()
-        preco_custo = validacao(("Coloque aqui o preço de custo desse produto: R$ ")).upper()
-        preco_venda = validacao(("Coloque aqui o preço de venda desse produto: R$ ")).upper()
+        quantidade = validacao_numerica(("Digite aqui a quantidade que foi reposta no estoque desse produto: "))
+        estoque_minimo = validacao_numerica("Digite aqui o estoque mínimo desse produto: ")
+        estoque_maximo = validacao_numerica("Digite aqui o estoque máximo desse produto: ")
+        preco_custo = validacao_numerica("Coloque aqui o preço de custo desse produto: R$ ",float)
+        preco_venda = validacao_numerica("Coloque aqui o preço de venda desse produto: R$ ",float)
         fornecedor = validacao("Digite aqui o fornecedor desse produto: ").upper()
-        data_validade = validacao(("Digite aqui a data de validade desse produto: ")).upper()
-        data = validacao("Digite aqui a data de atualização:  ").upper()
+        data_validade = validacao(("Digite aqui a data de validade desse produto: "))
+        data = validacao("Digite aqui a data de atualização:  ")
 
             # Dicionário de Dados.
 
@@ -49,17 +59,16 @@ def cadastrar():
             "estoque_maximo": estoque_maximo,
             "preco_custo": preco_custo,
             "preco_venda": preco_venda,
-            "fornecedor": fornecedor
-
+            "fornecedor": fornecedor,
+            "data_validade": data_validade,
+            "ultima_atualizacao": data
         }
 
             # Exportação para o Arquivo Estoque.
 
-        estoque.append(produto)
+        estoque[id] = produto
 
-        arquivo = open("estoque.json", "w")
-        json.dump(estoque, arquivo, indent=4)
-        arquivo.close()
+        salvar_estoque(estoque)
 
         print("Produto cadastrado!")
 
